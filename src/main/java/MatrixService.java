0e05d146b655799480c54530b37e69ff71eb9a23
0razzu <0razzu@protonmail.com>
2020-03-19 23:15:46 +0600

    MatrixService

---
+++ src/main/java/MatrixService.java
@@ -0,0 +1,10 @@
+import matrixes.IMatrix;
+
+import java.util.Arrays;
+
+
+public class MatrixService {
+    public static void arrangeMatrixes(IMatrix[] matrixes) {
+        Arrays.sort(matrixes, new MatrixComparator<>());
+    }
+}

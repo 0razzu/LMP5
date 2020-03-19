12a544843e1d735af7ac373b81e1e838f4f5314e
0razzu <0razzu@protonmail.com>
2020-03-19 22:41:35 +0600

    UpTriangleMatrix

---
+++ src/main/java/matrixes/square_matrixes/UpTriangleMatrix.java
@@ -0,0 +1,38 @@
+package matrixes.square_matrixes;
+
+
+import matrixes.MatrixErrorCode;
+
+import static java.lang.Math.abs;
+
+
+public class UpTriangleMatrix extends TriangleMatrix {
+    public UpTriangleMatrix(int dimension) {
+        super(dimension);
+        
+        data = new double[dimension * (dimension + 1) / 2];
+    }
+    
+    
+    @Override
+    public void setElem(int row, int column, double value) {
+        checkIndexes(row, column);
+    
+        if (abs(value) > EPS && row > column)
+            throw new ArrayIndexOutOfBoundsException(
+                    String.format(MatrixErrorCode.INCORRECT_INDEXES.getErrorString(), dimension, row, column));
+    
+        data[row * dimension + column - row * (row + 1) / 2] = value;
+    }
+    
+    
+    @Override
+    public double getElem(int row, int column) {
+        checkIndexes(row, column);
+        
+        if (row > column)
+            return 0;
+        
+        return data[row * dimension + column - row * (row + 1) / 2];
+    }
+}

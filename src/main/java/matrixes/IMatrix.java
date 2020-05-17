83ed01fc511e84a423aa44342b20db6dbf0dd187
0razzu <0razzu@protonmail.com>
2020-05-18 03:06:19 +0600

    formatting

--- src/main/java/matrixes/IMatrix.java
+++ src/main/java/matrixes/IMatrix.java
@@ -3,10 +3,7 @@ package matrixes;
 
 public interface IMatrix {
     void setElem(int row, int column, double value);
-    
-    
     double getElem(int row, int column);
-    
     
     double getDeterminant();
 }
